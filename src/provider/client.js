d2fe868f2c29ed9a636f679435aa436e5199994a
Gabriel <gbrei@hotmail.com>
2021-11-06 13:17:41 -0300

    feat: create request interceptor

--- src/provider/client.js
+++ src/provider/client.js
@@ -4,4 +4,14 @@ const client = axios.create({
 	baseURL: 'https://gtabackendozo.herokuapp.com',
 })
 
+client.interceptors.request.use(config => {
+	const token = localStorage.getItem('token')
+	console.log('request interceptada')
+
+	//verifica token no header, pra bater na api
+	if (token) config.headers.Authorization = `Bearer ${token}`
+
+	return config
+})
+
 export default client
